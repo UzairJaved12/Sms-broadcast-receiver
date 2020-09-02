42dba4d1b1235ab92b1b8057697d72200fdccc77
Uzair Javed <malik.hzu@gmail.com>
2020-09-02 15:32:05 +0500

    first commit

--- sample/src/main/java/com/kwabenaberko/smsbroadcastreceiver/MainActivity.java
+++ sample/src/main/java/com/kwabenaberko/smsbroadcastreceiver/MainActivity.java
@@ -1,15 +1,105 @@
 package com.kwabenaberko.smsbroadcastreceiver;
 
+import android.Manifest;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.provider.Settings;
+import android.provider.Telephony;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.telephony.SmsManager;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    // Defining Permission codes.
+    // We can give any value
+    // but unique for each permission.
+    private Button Sms;
 
-    public static final String TAG = MainActivity.class.getSimpleName();
+    private boolean sentToSettings = false;
+    private SharedPreferences permissionStatus;
+
+
+
+    private static final int PERMISSION_SEND_SMS = 123;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Sms = findViewById(R.id.btn_sms);
+
+        // Set Buttons on Click Listeners
+        Sms.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v)
+            {
+                checkPermission(
+                        Manifest.permission.READ_SMS,
+                        PERMISSION_SEND_SMS);
+            }
+        });
+
+    }
+
+    // Function to check and request permission.
+    public void checkPermission(String permission, int requestCode)
+    {
+        if (ContextCompat.checkSelfPermission(MainActivity.this, permission)
+                == PackageManager.PERMISSION_DENIED) {
+
+            // Requesting the permission
+            ActivityCompat.requestPermissions(MainActivity.this,
+                    new String[] { permission },
+                    requestCode);
+        }
+        else {
+            Toast.makeText(MainActivity.this,
+                    "Permission already granted",
+                    Toast.LENGTH_SHORT)
+                    .show();
+        }
+    }
+
+    // This function is called when the user accepts or decline the permission.
+    // Request Code is used to check which permission called this function.
+    // This request code is provided when the user is prompt for permission.
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           @NonNull String[] permissions,
+                                           @NonNull int[] grantResults)
+    {
+        super
+                .onRequestPermissionsResult(requestCode,
+                        permissions,
+                        grantResults);
+
+        if (requestCode == PERMISSION_SEND_SMS) {
+            if (grantResults.length > 0
+                    && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                Toast.makeText(MainActivity.this,
+                        "Sms Permission Granted",
+                        Toast.LENGTH_SHORT)
+                        .show();
+            }
+            else {
+                Toast.makeText(MainActivity.this,
+                        "Sms Permission Denied",
+                        Toast.LENGTH_SHORT)
+                        .show();
+            }
+        }
     }
 }
--- sample/src/main/java/com/kwabenaberko/smsbroadcastreceiver/MySmsReceiver.java
+++ sample/src/main/java/com/kwabenaberko/smsbroadcastreceiver/MySmsReceiver.java
@@ -1,20 +1,48 @@
 package com.kwabenaberko.smsbroadcastreceiver;
 
+import android.Manifest;
+import android.app.Activity;
+import android.content.pm.PackageManager;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.telephony.SmsMessage;
 import android.util.Log;
 
 import com.kwabenaberko.smsbroadcastreceiverlib.SmsBroadcastReceiver;
+
 
 /**
  * Created by Kwabena Berko on 8/2/2018.
  */
 
 public class MySmsReceiver extends SmsBroadcastReceiver {
+    private static final String TAG = "MySmsReceiver";
+
+
 
     @Override
     protected void onSmsReceived(SmsMessage smsMessage) {
-        Log.d(MainActivity.TAG, "ONSMSRECEIVED");
-        Log.d(MainActivity.TAG, smsMessage.getDisplayOriginatingAddress());
-        Log.d(MainActivity.TAG, smsMessage.getDisplayMessageBody());
+
+
+
+            Log.d(TAG, "ONSMSRECEIVED");
+            Log.d(TAG, smsMessage.getDisplayOriginatingAddress());
+            Log.d(TAG, smsMessage.getDisplayMessageBody());
+
+            String telemartOtpCord = smsMessage.getDisplayMessageBody();
+            String name = "Uzair";
+            String name2 = "Moeen";
+
+            /*   String newotp = telemartOtpCord.replace("Your Telemart OTP is: : ", "");*/
+            // Log.e(TAG,  newotp);
+
+            String currentString = "Your Telemart OTP is: 524824";
+            String[] separated = currentString.split(": ");
+            String value1 = separated[0];
+            String value2 = separated[1];
+            Log.e(TAG, value1 + value2);
+
+
+        }
     }
-}
+
